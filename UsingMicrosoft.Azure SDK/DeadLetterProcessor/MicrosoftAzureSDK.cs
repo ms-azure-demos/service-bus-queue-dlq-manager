1418fc45cd097a8fd5397f7c5323a18222be1d44
Joy George Kunjikkuru <joymon@gmail.com>
2022-04-02 12:26:53 -0400

    Initial check-in

---
+++ UsingMicrosoft.Azure SDK/DeadLetterProcessor/MicrosoftAzureSDK.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+
+using Microsoft.Azure.ServiceBus.Core;
+namespace DeadLetterProcessor
+{
+    class MicrosoftAzureSDK
+    {
+        internal static void ReSubmitDeadLetterMessageUsingMicrosoftAzureSDK(string connectionString, string queueName, int count)
+        {
+            int processed = 0;
+            Message message;
+            do
+            {
+                // here, we create a receiver on the Deadletter queue
+                var dlqReceiver = new Microsoft.Azure.ServiceBus.Core.MessageReceiver(connectionString,
+                EntityNameHelper.FormatDeadLetterPath(queueName),
+                Microsoft.Azure.ServiceBus.ReceiveMode.PeekLock);
+                dlqReceiver.ServiceBusConnection.TransportType = TransportType.AmqpWebSockets;
+
+                var resubmitSender = new MessageSender(connectionString, queueName);
+                resubmitSender.ServiceBusConnection.TransportType = TransportType.AmqpWebSockets;
+                // close the receiver and factory when the CancellationToken fires 
+                //cancellationToken.Register(
+                //    async () =>
+                //    {
+                //        await dlqReceiver.CloseAsync();
+                //        doneReceiving.SetResult(true);
+                //    });
+                // register the RegisterMessageHandler callback
+                message = dlqReceiver.ReceiveAsync().Result;
+                if (message != null)
+                {
+                    Console.WriteLine($"{nameof(MicrosoftAzureSDK)} - Received Message. Sequecen :{message.MessageId}");
+                    // first, we create a clone of the picked up message
+                    // that we can resubmit. 
+                    var resubmitMessage = message.Clone();
+                    // if the cloned message has an "error" we know the main loop
+                    // can't handle, let's fix the message
+                    if (resubmitMessage != null)
+                    {
+                        lock (Console.Out)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine($"{nameof(MicrosoftAzureSDK)} - Resubmitting: MessageId : {message.MessageId},SequenceNumber : {message.SystemProperties.SequenceNumber}, Label = {message.Label}");
+                            Console.ResetColor();
+                        }
+                        // set the label to "Scientist"
+                        // and re-enqueue the cloned message
+                        resubmitSender.SendAsync(resubmitMessage).Wait();
+                    }
+                    // finally complete the original message and remove it from the DLQ
+                    dlqReceiver.CompleteAsync(message.SystemProperties.LockToken).Wait();
+                    Console.WriteLine($"{nameof(MicrosoftAzureSDK)} - REsubmitted the deadletter message back to queue. MessageId: {message.MessageId}");
+                    processed++;
+                }
+                else
+                {
+                    Console.WriteLine("Not dequeued");
+                }
+            } while (message != null && processed < count);
+        } 
+
+        private static Task LogMessageHandlerException(ExceptionReceivedEventArgs e)
+        {
+            Console.WriteLine($"Exceptio {e.Exception}");
+            return Task.Delay(1);
+        }
+    }
+}
---
+++ UsingMicrosoft.Azure SDK/DeadLetterProcessor/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.ServiceBus.Messaging;
+using System.Transactions;
+
+namespace DeadLetterProcessor
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var connectionString = args[3];
+            var queueName = args[2];
+            var count = int.Parse(args[1]);
+            var option = int.Parse(args[0]);
+            Console.WriteLine($"Option:{option}, Count: {count}, queueName:{queueName}, connString:{connectionString}");
+
+            switch (option)
+            {
+                case 0:
+                    SimulateFailure(connectionString, queueName);
+                    return;
+                case 1:
+                    OldDK.DequeueMessage(connectionString, queueName, count);
+                    break;
+
+                case 2:
+
+                    //ReSubmitDeadLetterMessageUsingAzureSDK(connectionString, queueName, count);
+                    MicrosoftAzureSDK.ReSubmitDeadLetterMessageUsingMicrosoftAzureSDK(connectionString, queueName, count);
+                    //ReSubmitDeadLetterMessage(connectionString, queueName, count);
+                    break;
+            }
+            Console.ReadLine();
+        }
+        
+        private static void ReSubmitDeadLetterMessage(string connectionString, string queueName, int count)
+        {
+            var client = QueueClient.CreateFromConnectionString(connectionString, queueName);
+            BrokeredMessage originalMessage;
+            var queue = QueueClient.CreateFromConnectionString(connectionString, 
+                QueueClient.FormatDeadLetterPath(queueName), ReceiveMode.PeekLock);
+           
+            int processed = 0;
+            do
+            {
+                originalMessage = queue.Receive();
+                Console.WriteLine($"Message received from deadletter queue. SequenceNo:{originalMessage.SequenceNumber}");
+                if (originalMessage != null)
+                {
+                    using (var scope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
+                    {
+                        // Create new message
+                        var resubmittableMessage = originalMessage.Clone();
+
+                        // Remove dead letter reason and description
+                        resubmittableMessage.Properties.Remove("DeadLetterReason");
+                        resubmittableMessage.Properties.Remove("DeadLetterErrorDescription");
+                        
+                        // Resend cloned DLQ message and complete original DLQ message
+                        client.Send(resubmittableMessage);
+                        Console.WriteLine($"OriginalMessage {originalMessage.SequenceNumber} resumitted");
+                        originalMessage.Complete();
+
+                        // Complete transaction
+                        scope.Complete();
+                    }
+                }
+                processed++;
+            } while (originalMessage != null && processed < count);
+            Console.WriteLine($"Completed processing {processed}");
+        }
+
+        private static QueueClient SimulateFailure(string connectionString, string queueName)
+        {
+            BrokeredMessage originalMessage;
+            var queue = QueueClient.CreateFromConnectionString(connectionString, queueName, ReceiveMode.PeekLock);
+
+            originalMessage = queue.Receive();
+            throw new Exception("Forceful failure");
+            return queue;
+        }
+    }
+}
